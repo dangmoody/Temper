93417301e959bdf3d6854b833988b023cc2396fd
dan <daniel.guy.moody@gmail.com>
2021-01-09 17:51:17 +0000

    we decided this wasnt a good idea

--- demos/scale_test_c/scale_test_c.c
+++ demos/scale_test_c/scale_test_c.c
@@ -5,11 +5,6 @@
 
 //#ifdef TANTRUM_TESTS_ENABLED
 
-// DM: I've also got this exact entry point in my filtering demo
-// would it be worth making a TANTRUM_MAIN() so that if people just want to get a quick test application running then they can type that instead of all of this?
-// the only fear I have with that is people then might think trying to get more manual control over their entry points could be more difficult and they could be scared off
-// but I think documentation would solve that
-// what do we think? good idea? bad?
 int main( int argc, char** argv ){
 	TANTRUM_SETUP();
 	int returnValue = TANTRUM_RUN_ALL_TESTS_WITH_ARGS( argc, argv );
