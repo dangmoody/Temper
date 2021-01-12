053defc3dbce4504f509708ddd994210831fa0a2
dan <daniel.guy.moody@gmail.com>
2021-01-12 20:26:49 +0000

    this never happened

--- demos/threads/threads.c
+++ demos/threads/threads.c
@@ -1,7 +1,7 @@
 #include "../../tantrum.h"
 
 static void CheckIntDoesNotEqual( const uint32_t a, const uint32_t b ) {
-	TANTRUM_TEST_FALSE_OR_ABORT( a == b, "A is equal to B now!" );
+	TANTRUM_TEST_FALSE_OR_ABORT_M( a == b, "A is equal to B now!" );
 }
 
 TANTRUM_TEST( DanTest0, TANTRUM_TEST_FLAG_SHOULD_RUN ) {
@@ -12,13 +12,13 @@ TANTRUM_TEST( DanTest0, TANTRUM_TEST_FLAG_SHOULD_RUN ) {
 
 TANTRUM_TEST( DanTest1, TANTRUM_TEST_FLAG_SHOULD_RUN ) {
 	for ( uint32_t i = 0; i < 10; i++ ) {
-		TANTRUM_TEST_EQUAL( i, i, "NO" );
+		TANTRUM_TEST_EQUAL_M( i, i, "NO" );
 	}
 }
 
 TANTRUM_TEST( DanTest2, TANTRUM_TEST_FLAG_SHOULD_RUN ) {
 	for ( uint32_t i = 0; i < 10; i++ ) {
-		TANTRUM_TEST_NOT_EQUAL( i, i + 1, "NO2" );
+		TANTRUM_TEST_NOT_EQUAL_M( i, i + 1, "NO2" );
 	}
 }
 
