695c768fdace6b12f3ce745ec96a1d35b84dcc50
dan <daniel.guy.moody@gmail.com>
2021-01-14 21:41:04 +0000

    fix scale test compiler errors for c++

--- demos/scale_test_c/vector2d.c
+++ demos/scale_test_c/vector2d.c
@@ -1,35 +1,41 @@
 #include "vector2d.h"
 #include <math.h>
 
+#ifdef __cplusplus
+#define CONSTRUCT( type )	type
+#else
+#define CONSTRUCT( type )	( type )
+#endif
+
 //==========================================================
 
 vector2d Vector2D_Add( vector2d A, vector2d B ){
-	return ( vector2d ) { A.x + B.x, A.y + B.y };
+	return CONSTRUCT( vector2d ) { A.x + B.x, A.y + B.y };
 }
 
 //==========================================================
 
 vector2d Vector2D_Sub( vector2d A, vector2d B ){
-	return ( vector2d ) { A.x - B.x, A.y - B.y };
+	return CONSTRUCT( vector2d ) { A.x - B.x, A.y - B.y };
 }
 
 //==========================================================
 
 vector2d Vector2D_Mul( vector2d A, vector2d B ){
-	return ( vector2d ) { A.x* B.x, A.y* B.y };
+	return CONSTRUCT( vector2d ) { A.x* B.x, A.y* B.y };
 }
 
 //==========================================================
 
 vector2d Vector2D_Div( vector2d A, vector2d B ){
-	return ( vector2d ) { A.x / B.x, A.y / B.y };
+	return CONSTRUCT( vector2d ) { A.x / B.x, A.y / B.y };
 }
 
 //==========================================================
 
 vector2d Vector2D_Normalize( vector2d vector ){
 	float length = Vector2D_GetLength( vector );
-	return ( vector2d ) {vector.x / length, vector.y / length};
+	return CONSTRUCT( vector2d ) {vector.x / length, vector.y / length};
 }
 
 //==========================================================
