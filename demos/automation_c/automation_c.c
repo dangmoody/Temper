2ee0fdffab4dc45ad4b9a1238cac25a5428b235d
Mike430 <mikedotcoder@gmail.com>
2021-01-17 00:18:28 +0000

    Made the automation_c file compile again. Changed the comment above the include.

--- demos/automation_c/automation_c.c
+++ demos/automation_c/automation_c.c
@@ -1,32 +1,31 @@
-// MY: TODO - should we leave this as is Dan or would you rather we added a compiler argument so it's even less explicitly documented
-#define TEMPER_SELF_TEST_ENABLED
+#define TEMPER_SELF_TEST_ENABLED // This is for testing temper only. Please don't add this in your production environment
 
 #include "../../temper.h"
 
 //----------------------------------------------------------
 
-TEMPER_TEST(GivenIsolatedTest_WhenDeclared_IsExecuted, false, false, TEMPER_TEST_FLAG_SHOULD_RUN)
+TEMPER_TEST(GivenIsolatedTest_WhenDeclared_IsExecuted, TEMPER_TEST_EXPECTATION_NONE, TEMPER_TEST_FLAG_SHOULD_RUN)
 {
 	TEMPER_TEST_TRUE(true);
 }
 
 //----------------------------------------------------------
 
-TEMPER_TEST( GivenIsolatedTest_WhenDeclaredButHasError_IsExecutedAndFails, true, false, TEMPER_TEST_FLAG_SHOULD_RUN )
+TEMPER_TEST( GivenIsolatedTest_WhenDeclaredButHasError_IsExecutedAndFails, TEMPER_TEST_EXPECTATION_FLAG_FAIL, TEMPER_TEST_FLAG_SHOULD_RUN )
 {
 	TEMPER_TEST_TRUE_M( false, "We expect this test to fail." );
 }
 
 //----------------------------------------------------------
 
-TEMPER_TEST( GivenIsolatedTest_WhenDeclaredButWillAbort_IsExecutedAndAborts, false, true, TEMPER_TEST_FLAG_SHOULD_RUN )
+TEMPER_TEST( GivenIsolatedTest_WhenDeclaredButWillAbort_IsExecutedAndAborts, TEMPER_TEST_EXPECTATION_FLAG_ABORT, TEMPER_TEST_FLAG_SHOULD_RUN )
 {
 	TEMPER_TEST_TRUE_OR_ABORT_M( false, "We expect this test to abort." );
 }
 
 //----------------------------------------------------------
 
-TEMPER_TEST( GivenIsolatedTest_WhenDeclaredWithErrorAndAbort_IsExecutedFailsAndAborts, true, true, TEMPER_TEST_FLAG_SHOULD_RUN )
+TEMPER_TEST( GivenIsolatedTest_WhenDeclaredWithErrorAndAbort_IsExecutedFailsAndAborts, TEMPER_TEST_EXPECTATION_FLAG_FAIL | TEMPER_TEST_EXPECTATION_FLAG_ABORT, TEMPER_TEST_FLAG_SHOULD_RUN )
 {
 	TEMPER_TEST_TRUE_M(false, "this failure is expected");
 	TEMPER_TEST_TRUE_OR_ABORT_M( false, "We expect this test to abort." );
@@ -34,14 +33,14 @@ TEMPER_TEST( GivenIsolatedTest_WhenDeclaredWithErrorAndAbort_IsExecutedFailsAndA
 
 //----------------------------------------------------------
 
-TEMPER_TEST( GivenIsolatedTest_WhenDeclaredWithSkipFlag_IsNotExecuted, false, false, TEMPER_TEST_FLAG_SHOULD_SKIP )
+TEMPER_TEST( GivenIsolatedTest_WhenDeclaredWithSkipFlag_IsNotExecuted, TEMPER_TEST_EXPECTATION_NONE, TEMPER_TEST_FLAG_SHOULD_SKIP )
 {
 	TEMPER_TEST_TRUE_M( false, "The 'skip' flag is broken. This should NOT have run!" );
 }
 
 //----------------------------------------------------------
 
-TEMPER_TEST( GivenIsolatedTest_WhenDeclaredWithDepricatedFlag_IsNotExecuted, false, false, TEMPER_TEST_FLAG_DEPRECATED )
+TEMPER_TEST( GivenIsolatedTest_WhenDeclaredWithDepricatedFlag_IsNotExecuted, TEMPER_TEST_EXPECTATION_NONE, TEMPER_TEST_FLAG_DEPRECATED )
 {
 	TEMPER_TEST_TRUE_M( false, "The 'depricated' flag is broken. This should NOT have run!" );
 }
@@ -52,7 +51,7 @@ int main( int argc, char** argv )
 {
 	TEMPER_SETUP();
 	int returnValue = TEMPER_RUN_ALL_TESTS_WITH_ARGS( argc, argv );
-	getchar();
+	//getchar();
 	return returnValue;
 }
 
